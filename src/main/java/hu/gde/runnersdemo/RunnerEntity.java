b0e04e24c0b260ee1b6694060597e2898555a3f9
Csorba David <csorbadavid@outlook.hu>
2023-05-25 17:54:56 +0200

    Extended runnerEntity with height field and added them at init. Created a new runner

--- src/main/java/hu/gde/runnersdemo/RunnerEntity.java
+++ src/main/java/hu/gde/runnersdemo/RunnerEntity.java
@@ -11,6 +11,7 @@ public class RunnerEntity {
     private long runnerId;
     private String runnerName;
     private long averagePace;
+    private int height;
 
     @OneToMany(mappedBy = "runner", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<LapTimeEntity> laptimes = new ArrayList<>();
@@ -31,6 +32,10 @@ public class RunnerEntity {
         return averagePace;
     }
 
+    public int getHeight() {
+        return height;
+    }
+
     public void setRunnerId(long runnerId) {
         this.runnerId = runnerId;
     }
@@ -41,6 +46,10 @@ public class RunnerEntity {
 
     public void setAveragePace(long averagePace) {
         this.averagePace = averagePace;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 
     public List<LapTimeEntity> getLaptimes() {
